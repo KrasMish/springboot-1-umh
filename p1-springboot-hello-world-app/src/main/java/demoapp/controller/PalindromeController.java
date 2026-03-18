4607fbd26051a3019242c8ccf057c5fd9c54bae6
KrasMish <mikekrasin2005@gmail.com>
2026-03-18 13:49:25 +0200

    add palindromecontroller

---
+++ p1-springboot-hello-world-app/src/main/java/demoapp/controller/PalindromeController.java
@@ -0,0 +1,40 @@
+package demoapp.controller;
+
+import demoapp.model.PalindromeData;
+import demoapp.service.PalindromeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class PalindromeController {
+
+    @Autowired
+    private PalindromeService service;
+
+    // Show form
+    @GetMapping("/palindrome")
+    public String showForm(PalindromeData data) {
+        return "palindromeForm";
+    }
+
+    // Process form
+    @PostMapping("/palindrome")
+    public String checkPalindrome(@ModelAttribute @Valid PalindromeData data,
+                                  BindingResult result,
+                                  Model model) {
+
+        if (result.hasErrors()) {
+            return "palindromeForm";
+        }
+
+        boolean isPalindrome = service.isPalindrome(data.getWord());
+
+        model.addAttribute("result", isPalindrome);
+        return "palindromeResult";
+    }
+}
